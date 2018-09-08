688f4fce4312be2a38e00658e3e3f266d869dee7
ehterwie <ehterwie@uwaterloo.ca>
2018-09-08 11:30:49 -0400

    Got some stuff with fires working

--- mobile/app/screens/Home.js
+++ mobile/app/screens/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Circle } from "react-native-maps";
 
 import bottle from "../bottle";
 
@@ -18,13 +18,40 @@ class HomeScreen extends Component {
 
   render() {
     console.log(this.state.fires);
+
+    const fires = this.state.fires.map((fire, index) => (
+        <Circle
+          key={index}
+          center={{
+            latitude: parseFloat(fire.latitude),
+            longitude: parseFloat(fire.longitude),
+          }}
+          radius={fire.radius * 10}
+          fillColor="rgba(255, 0, 0, 0.5)"
+        />
+    ));
+
+
+    // const fires = this.state.fires[0] ? (
+    //   <Circle
+    //     center={{
+    //       latitude: 49.0,
+    //       longitude: -120.0,
+    //     }}
+    //     radius={1000000}
+    //     fillColor="rgba(255, 0, 0, 0.5)"
+    //   />
+    // ) : null;
+
     return (
       <MapView
         style={styles.map}
         provider="google"
         mapType="hybrid"
         showsUserLocation={true}
-      />
+      >
+        {fires}
+      </MapView>
     )
   }
 }
